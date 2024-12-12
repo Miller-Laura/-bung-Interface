15b55f807419d4b39522f6e49f92a8a7595b0c77
Wesley Rubio Cueva <wesley.rubio.cueva@gdata.de>
2024-12-12 13:17:16 +0100

    added library for declarative programming

---
+++ Angular-Playground/platform/projects/declarative-programming/src/lib/declarative-programming.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-declarative-programming',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      declarative-programming works!
+    </p>
+  `,
+  styles: ``
+})
+export class DeclarativeProgrammingComponent {
+
+}
---
+++ Angular-Playground/platform/projects/declarative-programming/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of declarative-programming
+ */
+
+export * from './lib/declarative-programming.service';
+export * from './lib/declarative-programming.component';
