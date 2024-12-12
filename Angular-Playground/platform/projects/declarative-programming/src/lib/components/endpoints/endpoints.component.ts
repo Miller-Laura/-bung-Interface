382f28414d502a7373e3fe48d6039e9f689bf3a9
Wesley Rubio Cueva <wesley.rubio.cueva@gdata.de>
2024-12-12 17:23:42 +0100

    add declarative programming example

--- Angular-Playground/platform/projects/declarative-programming/src/lib/components/endpoints/endpoints.component.ts
+++ Angular-Playground/platform/projects/declarative-programming/src/lib/components/endpoints/endpoints.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, Signal } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { DeclarativeProgrammingService, Fruit } from '../../declarative-programming.service';
+import { BehaviorSubject, combineLatest, debounceTime, distinctUntilChanged, map, Observable, shareReplay } from 'rxjs';
 
 @Component({
   selector: 'lib-endpoints',
@@ -7,4 +10,24 @@ import { Component } from '@angular/core';
   templateUrl: './endpoints.component.html',
   styleUrl: './endpoints.component.scss',
 })
-export class EndpointsComponent {}
+export class EndpointsComponent {
+  private readonly _searchValue$: BehaviorSubject<string> = new BehaviorSubject('');
+  private readonly service = inject(DeclarativeProgrammingService);
+
+  private readonly _fruits$: Observable<Array<Fruit>> = this.service.getFruits()
+    .pipe(
+      shareReplay(1)
+    );
+  private readonly _filteredFruits$: Observable<Array<Fruit>> = combineLatest([this._fruits$, this._searchValue$])
+    .pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      map(([fruits, searchValue]) => fruits.filter((fruit) => fruit.name.toLowerCase().includes(searchValue.toLowerCase()))),
+    );
+
+  public readonly filteredFruits: Signal<Array<Fruit> | undefined> = toSignal(this._filteredFruits$);
+
+  public setSearchValue(value: string): void {
+    this._searchValue$.next(value);
+  }
+}
--- Angular-Playground/platform/projects/declarative-programming/src/lib/declarative-programming.service.ts
+++ Angular-Playground/platform/projects/declarative-programming/src/lib/declarative-programming.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, of } from 'rxjs';
+
+export interface Fruit {
+  name: string;
+  colour: string;
+}
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DeclarativeProgrammingService {
+  private fruits$ = new BehaviorSubject<Array<Fruit>>([
+    { name: 'Apple', colour: 'Red' },
+    { name: 'Banana', colour: 'Yellow' },
+    { name: 'Orange', colour: 'Orange' },
+    { name: 'Grapes', colour: 'Purple' },
+    { name: 'Pineapple', colour: 'Brown' },
+    { name: 'Strawberry', colour: 'Red' },
+    { name: 'Watermelon', colour: 'Green' },
+    { name: 'Blueberry', colour: 'Blue' },
+  ]);
 
-  constructor() { }
+  public getFruits(): Observable<Array<Fruit>> {
+    return this.fruits$.asObservable();
+  }
 }
