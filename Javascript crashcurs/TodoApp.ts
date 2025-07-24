7ce5b03488a46a3fc840c065002d21e806e4af94
Laura Miller <l.miller.2001@web.de>
2025-07-24 13:59:52 +0200

    added data and corrected code

--- Javascript crashcurs/TodoApp.ts
+++ Javascript crashcurs/TodoApp.ts
@@ -13,35 +13,35 @@ interface ToDo {
 }
 
 //2. Initialisierung
-let todos: ToDo[];
+let todos: ToDo[] = [];
 let nextId = 1;
 
 //3. Todo hinzufügen (Spread Operator & Arrow Function)
-const addTodo = (task: string, priority: Priority): void => {
+export const addTodo = (task: string, priority: Priority): void => {
   todos = [...todos, { id: nextId++, task, completed: false, priority }];
 };
-addTodo("Todo hinzufügen", Priority.medium);
+addTodo("Jon in den Kaffee spucken", Priority.high);
 
 //4. Todo als erledigt makieren(map, spread, ternary)
-const completTodo = (id: number): void => {
+export const completedTodo = (id: number): void => {
   todos = todos.map((todo) =>
     todo.id === id ? { ...todo, completed: true } : todo
   );
 };
-const doneTodo = (id: number) => (id = id);
 
 //5. Todo löschen (filter & Arrow Function)
-const deleteTodo = (id: number): ToDo[] => {
+export const deleteTodo = (id: number): ToDo[] => {
   return todos.filter((todo) => todo.id === id);
 };
 
 //6. Todos anzeigen (forEach, Ternary, Template Strings) //1. leere liste, todo in liste eintragen, danach liste abhacken oder löschen, neue todo eintragen
-const showTodos = (): void => {
-  todos.forEach((currentTodo: ToDo) => {
-    if (currentTodo.completed) {
-      console.log("[x] Todo ist erledigt");
-    } else console.log("[ ] Todo ist nicht erledigt");
-  }); //(method) Array<ToDo>.forEach(callbackfn: (value: ToDo, index: number, array: ToDo[]) => void, thisArg?: any): void
+export const showTodos = (): void => {
+  todos.forEach((todo) => {
+    const checkTodo = todo.completed
+      ? "Todo ist erledigt [x]"
+      : "Todo ist noch offen [ ]";
+    console.log(checkTodo);
+  });
 };
 
 //7. Testdaten und Funktionsaufrufe
@@ -60,8 +60,14 @@ type myToDo = ToDo;
   {
     task: "Pflanzen gießen",
     completed: false,
-    priority: "medium",
+    priority: "low",
+  },
+  { task: "Vier Gewinnt erstellen", completed: false, Priority: "medium" },
+  {
+    task: "Jon ärgern",
+    completed: false,
+    Priority: "high",
   },
 ];
 //const showTodos: void = completTodo(myToDo );
-console.log(completTodo);
+console.log(completedTodo);
---
+++ Javascript crashcurs/button.ts
@@ -0,0 +1,4 @@
+/*Schreibe eine Funktion, 
+die beim Klick auf den Button den Text aus dem Eingabefeld nimmt und 
+als neues Listenelement (li) zur Liste hinzufügt.*/
+document.getElementById("addTodo") as HTMLButtonElement;
