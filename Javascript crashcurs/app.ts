3e912c9ed83da41414f58be43b90231e0ea75764
Laura Miller <l.miller.2001@web.de>
2025-07-29 14:30:15 +0200

    added if-function, changed colors and fixed code

--- Javascript crashcurs/app.ts
+++ Javascript crashcurs/app.ts
@@ -1,18 +1,24 @@
-const addBtn = document.getElementById("addBtn") as HTMLButtonElement;
-const taskInput = document.getElementById("taskInput") as HTMLInputElement;
-const taskList = document.getElementById("taskList") as HTMLUListElement;
+const addBtn = document.getElementById("addBtn");
+const taskInput = document.getElementById("taskInput");
+const taskList = document.getElementById("taskList");
 
-addBtn.addEventListener("click", () => {
-  const taskText = taskInput.value.trim();
-  if (taskText !== "") {
-    const li = document.createElement("li");
-    li.textContent = taskText;
-    taskList.appendChild(li);
-    taskInput.value = ""; // Eingabefeld leeren
-  }
-});
-const deleteBtn = document.getElementById("deleteBtn") as HTMLButtonElement;
+if (addBtn) {
+  addBtn.addEventListener("click", () => {
+    const taskText = (taskInput as HTMLInputElement).value.trim();
+    if (taskText !== "") {
+      const li = document.createElement("li");
+      li.textContent = taskText;
+      taskList?.appendChild(li);
+      (taskInput as HTMLInputElement).value = ""; // Eingabefeld leeren
+    }
+  });
+}
+const deleteBtn = document.getElementById("deleteBtn");
 
-deleteBtn.addEventListener("click", () => {
-  taskList.innerHTML = ""; // Liste leeren
-});
+if (deleteBtn) {
+  deleteBtn.addEventListener("click", () => {
+    if (taskList) {
+      taskList.innerHTML = ""; // Liste leeren
+    }
+  });
+}
